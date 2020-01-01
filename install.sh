abe80d6bb798383d32dba7ab592545b12e6ff649
Maor Leger <maor.leger@gmail.com>
2020-01-01 12:38:10 -0800

    install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+wget -qO - https://apt.thoughtbot.com/thoughtbot.gpg.key | sudo apt-key add -
+echo "deb https://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
+sudo apt-get update
+sudo apt-get install -y rcm
+
+env RCRC=$HOME/dotfiles/rcrc rcup
