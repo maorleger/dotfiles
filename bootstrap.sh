2debfcb10f02115673b8333d89d10629e74f8930
Maor Leger <maorleger@microsoft.com>
2024-01-31 14:29:25 -0800

    continute codespaces config: verbose, skip pre-hook, set directories via command line

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,14 +3,13 @@
 set -e
 
 echo 'starting dotfiles bootstrap'
+
 # Install rcm
 echo 'installing rcm'
 sudo apt update
 sudo apt install rcm
 
-echo 'starting dotfiles bootstrap'
-
 BASEDIR=$(realpath $(dirname "$0"))
 
 echo "rcrc located in $BASEDIR"
-env RCRC="$BASEDIR"/rcrc rcup
+env RCRC="$BASEDIR"/rcrc rcup -K -vvv -f -d "$BASEDIR" -d "$BASEDIR"/dotfiles-local
