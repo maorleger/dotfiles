6c59eeb3bcc08e5560625573d0638c7a72b03f91
Maor Leger <maorleger@microsoft.com>
2024-01-31 15:13:48 -0800

    update script

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,9 +7,9 @@ echo 'starting dotfiles bootstrap'
 # Install rcm
 echo 'installing rcm'
 sudo apt update
-sudo apt install rcm
+sudo apt install rcm -y
 
 BASEDIR=$(realpath $(dirname "$0"))
 
 echo "rcrc located in $BASEDIR"
-env RCRC="$BASEDIR"/rcrc rcup -K -vvv -f -d "$BASEDIR" -d "$BASEDIR"/dotfiles-local
+env RCRC="$BASEDIR"/rcrc rcup -v -f -d "$BASEDIR" -d "$BASEDIR"/dotfiles-local
